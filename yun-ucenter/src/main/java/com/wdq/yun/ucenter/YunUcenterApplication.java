d1264d30d89b47a443e7551591a004ebee34ff21
wudingqiu <wudingqiu@fkhwl.com>
2018-12-29 16:47:04 +0800

    feign相关实现

---
+++ yun-ucenter/src/main/java/com/wdq/yun/ucenter/YunUcenterApplication.java
@@ -0,0 +1,14 @@
+package com.wdq.yun.ucenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunUcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunUcenterApplication.class, args);
+    }
+
+}
+
