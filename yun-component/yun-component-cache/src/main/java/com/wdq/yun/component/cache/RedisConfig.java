6b213ca51d7d5b8b70d3c44d130c5da7b9772593
wudingqiu <wudingqiu@fkhwl.com>
2019-08-23 10:18:41 +0800

    redis 连接池、工厂配置修改

--- yun-component/yun-component-cache/src/main/java/com/wdq/yun/component/cache/RedisConfig.java
+++ yun-component/yun-component-cache/src/main/java/com/wdq/yun/component/cache/RedisConfig.java
@@ -3,7 +3,6 @@ package com.wdq.yun.component.cache;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -12,12 +11,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 /**
@@ -32,17 +33,17 @@ import redis.clients.jedis.JedisPoolConfig;
 public class RedisConfig {
 
     @Autowired
-    private BeanFactory beanFactory;
-
-    @Autowired
     private RedisBaseConfig redisBaseConfig;
-
 
     @Bean
     public RedisBaseConfig redisBaseConfig() {
         return new RedisBaseConfig();
     }
 
+    /**
+     * 连接池配置
+     * @return
+     */
     @Bean(name="jedisPoolConfig")
     public JedisPoolConfig getJedisPoolConfig() {
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
@@ -58,16 +59,32 @@ public class RedisConfig {
     }
 
 
+    /**
+     * 连接工厂
+     * @param jedisPoolConfig
+     * @return
+     */
     @Bean
-    public JedisPool jedisPool() {
-        JedisPoolConfig config = (JedisPoolConfig)beanFactory.getBean("jedisPoolConfig");
-        return new JedisPool(config,redisBaseConfig.getHost(),redisBaseConfig.getPort());
+    public RedisConnectionFactory redisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+        //单机配置
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisBaseConfig.getHost());
+        config.setPort(redisBaseConfig.getPort());
+        config.setDatabase(redisBaseConfig.getDatabase());
+        //客户端配置（使用连接池）
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.
+                                                                builder().
+                                                                usePooling().
+                                                                poolConfig(jedisPoolConfig).
+                                                                build();
+        //单机配置 + 连接池
+        RedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory(config,jedisClientConfiguration);
+        return redisConnectionFactory ;
     }
 
     @Bean
     @ConditionalOnMissingBean(name = "redisTemplate")
-    public RedisTemplate<Object, Object> redisTemplate(
-        RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         //使用fastjson序列化
         FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
@@ -77,17 +94,14 @@ public class RedisConfig {
         // key的序列化采用StringRedisSerializer
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-//        template.setConnectionFactory(factory);
         template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
 
     @Bean
     @ConditionalOnMissingBean(StringRedisTemplate.class)
-    public StringRedisTemplate stringRedisTemplate(
-        RedisConnectionFactory redisConnectionFactory) {
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         StringRedisTemplate template = new StringRedisTemplate();
-//        template.setConnectionFactory(factory);
         template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
